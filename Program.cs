ebd152ff1a1cfcdaee1a0d8cf37c2ca30275c74d
juanabaunzabosch <juan.abaunza@mx.bosch.com>
2025-09-24 09:20:21 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,20 +1,43 @@
 using System;
-using System.Linq;
 using System.Collections.Generic;
 
 class Program
 {
     static void Main()
     {
-        List<string> names = new List<string>
+        List<string> names = GetNames();
+        List<string> filteredNames = GetNamesStartingWith(names, 'M');
+
+        PrintNames("Names starting with 'M':", filteredNames);
+    }
+
+    static List<string> GetNames()
+    {
+        return new List<string>
         {
             "Bosch", "Mexico", "Mango", "Mark", "Blr", "Clean code"
         };
+    }
 
-        var namesStartingWithM = names.Where(name => name.StartsWith("M")).ToList();
+    static List<string> GetNamesStartingWith(List<string> names, char initial)
+    {
+        List<string> result = new List<string>();
 
-        Console.WriteLine("Names starting with 'M':");
-        foreach (var name in namesStartingWithM)
+        foreach (string name in names)
+        {
+            if (!string.IsNullOrEmpty(name) && name.StartsWith(initial.ToString()))
+            {
+                result.Add(name);
+            }
+        }
+
+        return result;
+    }
+
+    static void PrintNames(string title, List<string> names)
+    {
+        Console.WriteLine(title);
+        foreach (string name in names)
         {
             Console.WriteLine(name);
         }
